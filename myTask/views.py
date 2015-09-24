9d01d6220d34c51bfd01fd6bd4f4669f568ce149
Kelvin Gomo <kelvingomo@gmail.com>
2015-09-24 16:47:54 +0200

    Added the insertion of a new task functionality.   - myTask/views.py   - myTask/templates/

--- myTask/views.py
+++ myTask/views.py
@@ -1,5 +1,5 @@
 import sqlite3
-from flask import render_template
+from flask import render_template, request, url_for
 from myTask import app
 
 @app.route('/')
@@ -20,6 +20,7 @@ def getTasks():
     # return data as a string
     return str(result)
 
+
 @app.route('/temp', methods=['GET'])
 def getTemplate():
     """
@@ -34,6 +35,30 @@ def getTemplate():
     result = cursor.fetchall()
     # close the db connection
     conn.close()
-    # return data as a string
-    return render_template('test.html', rows=result)
+    return render_template('makeTable.html', rows=result)
+
+
+@app.route('/new', methods=['GET', 'POST'])
+def newTask():
+    """
+    """
+    if request.method == 'POST':
+        newTaskName = request.form['task']
+        newDescr = request.form['description']
+        conn = sqlite3.connect(app.config['DATABASE'])
+        cursor = conn.cursor()
+
+        cursor.execute("INSERT INTO todo (task_name, description, status) VALUES "\
+                       "(?,?,?)", (newTaskName, newDescr, 1))
+
+        newID = cursor.lastrowid
+        conn.commit()
+        cursor.close()
+
+        resp = '<p>The new task was inserted into the database, the ID is: ' \
+                '%s</p>' % newID
+
+        return resp
+    else:
+        return render_template('newTask.html')
 
